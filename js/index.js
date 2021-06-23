f5db95a4fce137c352cb90395c40ecc7b96bde7d
bibekoli <bibekoli3@gmail.com>
2021-06-23 21:46:11 +0545

    update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
-document.querySelector(".aone").innerHTML = `
-<div class="atitle">
+if(true) {
+  s = `<div class="atitle">
   <span>Recently Posted</span>
-</div>
+</div>`;
+  t= `<div class="atitle">
+  <span>Most Popular</span>
+</div>`;
+a = `
+
 <div class="aitem" onclick="location.href = '/posts/build-android-apps-with-java-and-xml-on-android.html'">
             <div class="alogo">
               <img src="/thumbs/aide.jpg" alt="Build Android Apps with Java and XML using Android Phone" />
@@ -180,187 +185,14 @@ document.querySelector(".aone").innerHTML = `
           </div>
         
 `;
+}
+
+ao = s + a;
+ab = t + a;
+abb= t + a + a;
 
 
+document.querySelector(".aone").innerHTML = ao;
+document.querySelector(".atwo").innerHTML = ab;
+document.querySelector(".athree").innerHTML = abb;
 
-document.querySelector(".atwo").innerHTML = `
-<div class="atitle">
-  <span>Most Popular</span>
-</div>
-<div class="aitem" onclick="location.href = '/posts/build-android-apps-with-java-and-xml-on-android.html'">
-            <div class="alogo">
-              <img src="/thumbs/aide.jpg" alt="Build Android Apps with Java and XML using Android Phone" />
-            </div>
-            <div class="adesc">
-              <span>Build Android Apps with Java and XML using Android Phone</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/block-all-ads-on-rooted-android.html'">
-            <div class="alogo">
-              <img src="/thumbs/adaway.jpg" alt="Block All Ads On Apps and Websites on Rooted Android" />
-            </div>
-            <div class="adesc">
-              <span>Block All Ads On Apps and Websites on Rooted Android</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/run-c-sharp-programs-offline-on-android.html'">
-            <div class="alogo">
-              <img src="/thumbs/cs.jpg" alt="Run C# programs Offline On Your Android" />
-            </div>
-            <div class="adesc">
-              <span>Run C# programs Offline On Your Android</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/run-kotlin-programs-offline-on-android.html'">
-            <div class="alogo">
-              <img src="/thumbs/kt.jpg" alt="Run Kotlin programs Offline On Your Android" />
-            </div>
-            <div class="adesc">
-              <span>Run Kotlin programs Offline On Your Android</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/run-ruby-programs-offline-on-android.html'">
-            <div class="alogo">
-              <img src="/thumbs/ruby.jpg" alt="Run Ruby programs Offline On Your Android" />
-            </div>
-            <div class="adesc">
-              <span>Run Ruby programs Offline On Your Android</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/run-c-and-cpp-programs-offline-on-android.html'">
-            <div class="alogo">
-              <img src="/thumbs/ccpp.jpg" alt="Run C and C++ programs Offline On Your Android" />
-            </div>
-            <div class="adesc">
-              <span>Run C and C++ programs Offline On Your Android</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/best-code-editor-apps-and-html-ide-for-android.html'">
-            <div class="alogo">
-              <img src="/thumbs/editor.jpg" alt="Best Code Editor Apps and HTML IDE for Android" />
-            </div>
-            <div class="adesc">
-              <span>Best Code Editor Apps and HTML IDE for Android</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/best-free-online-spelling-and-grammar-checkers.html'">
-            <div class="alogo">
-              <img src="/thumbs/spell.jpg" alt="Check Spelling and Grammar Of Your Text Online" />
-            </div>
-            <div class="adesc">
-              <span>Check Spelling and Grammar Of Your Text Online</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/save-and-access-clipboard-history.html'">
-            <div class="alogo">
-              <img src="/thumbs/copy.jpg" alt="Save and Access Your Clipboard History On Android" />
-            </div>
-            <div class="adesc">
-              <span>Save and Access Your Clipboard History On Android</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/delete-instagram-account.html'">
-            <div class="alogo">
-              <img src="/thumbs/igdl.jpg" alt="Permanently Delete Your Instagram Account" />
-            </div>
-            <div class="adesc">
-              <span>Permanently Delete Your Instagram Account</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/check-and-remove-pending-follow-requests-on-instagram.html'">
-            <div class="alogo">
-              <img src="/thumbs/igfr.jpg" alt="Check and Remove Pending Follow Request on Instagram" />
-            </div>
-            <div class="adesc">
-              <span>Check and Remove Pending Follow Request on Instagram</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/block-all-ads-on-non-rooted-android.html'">
-            <div class="alogo">
-              <img src="/thumbs/block-this.jpg" alt="Block All Ads On Apps and Websites on Android" />
-            </div>
-            <div class="adesc">
-              <span>Block All Ads On Apps and Websites on Android</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/restore-twrp-backup-of-your-phone.html'">
-            <div class="alogo">
-              <img src="/thumbs/restoretwrp.jpg" alt="Restore TWRP Backup of Your Phone Along With Data and Apps" />
-            </div>
-            <div class="adesc">
-              <span>Restore TWRP Backup of Your Phone Along With Data and Apps</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/run-python-programs-offline-on-android.html'">
-            <div class="alogo">
-              <img src="/thumbs/python.jpg" alt="Run Python programs Offline On Your Android" />
-            </div>
-            <div class="adesc">
-              <span>Run Python programs Offline On Your Android</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/run-dart-programs-offline-on-android.html'">
-            <div class="alogo">
-              <img src="/thumbs/dart.jpg" alt="Run Dart programs Offline On Your Android" />
-            </div>
-            <div class="adesc">
-              <span>Run Dart programs Offline On Your Android</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/run-java-programs-offline-on-android.html'">
-            <div class="alogo">
-              <img src="/thumbs/java.jpg" alt="Run Java programs Offline On Your Android" />
-            </div>
-            <div class="adesc">
-              <span>Run Java programs Offline On Your Android</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/run-vb.net-programs-offline-on-android.html'">
-            <div class="alogo">
-              <img src="/thumbs/vb.jpg" alt="Run VB.NET programs Offline On Your Android" />
-            </div>
-            <div class="adesc">
-              <span>Run VB.NET programs Offline On Your Android</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/deactivate-instagram-account.html'">
-            <div class="alogo">
-              <img src="/thumbs/igde.jpg" alt="Temporarily Deactivate Your Instagram Account" />
-            </div>
-            <div class="adesc">
-              <span>Temporarily Deactivate Your Instagram Account</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/capture-long-screenshot-of-apps-and-website-on-android.html'">
-            <div class="alogo">
-              <img src="/thumbs/longss.jpg" alt="Capture Long Screenshot of Apps and Website on Android" />
-            </div>
-            <div class="adesc">
-              <span>Capture Long Screenshot of Apps and Website on Android</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/change-screen-resolution-of-your-android-phone.html'">
-            <div class="alogo">
-              <img src="/thumbs/resolution.jpg" alt="Change Screen Resolution Of Your Android Device" />
-            </div>
-            <div class="adesc">
-              <span>Change Screen Resolution Of Your Android Device</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/create-twrp-backup-of-your-phone.html'">
-            <div class="alogo">
-              <img src="/thumbs/backuptwrp.jpg" alt="Create TWRP Backup of Your Phone Along With Data and Apps" />
-            </div>
-            <div class="adesc">
-              <span>Create TWRP Backup of Your Phone Along With Data and Apps</span>
-            </div>
-          </div>
-          <div class="aitem" onclick="location.href = '/posts/reboot-android-to-recovery-mode.html'">
-            <div class="alogo">
-              <img src="/thumbs/reboottorecovery.jpg" alt="Reboot an Android Device to Recovery Mode" />
-            </div>
-            <div class="adesc">
-              <span>Reboot an Android Device to Recovery Mode</span>
-            </div>
-          </div>
-        `;
